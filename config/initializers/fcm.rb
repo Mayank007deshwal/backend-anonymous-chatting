501d7ecbdf8ebbdc703f2308dc0d6b20d86e8953
Mayank <mayank007deshwal@gmail.com>
2024-08-29 16:25:16 +0530

    move fcm credential file to root directory

--- config/initializers/fcm.rb
+++ config/initializers/fcm.rb
@@ -2,6 +2,6 @@ require 'fcm'
 
 FCM_CLIENT = FCM.new(
   ENV['FCM_SERVER_KEY'],
-  Rails.root.join('config', 'test-pro-007-cd2a9a4d746f.json').to_s,
+  Rails.root.join('test-pro-007-cd2a9a4d746f.json').to_s,
   "test-pro-007"
 )
