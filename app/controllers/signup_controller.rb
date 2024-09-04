92b455c2d5699dc731b77b7b0a57c4cd6a5ae770
Mayank <mayank007deshwal@gmail.com>
2024-09-04 17:50:36 +0530

    fix cookies issue

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -21,8 +21,9 @@ class SignupController < ApplicationController
       value: get_token,
       httponly: true,   # Makes the cookie HTTP-only
       secure: Rails.env.production?,  # Ensure it's only sent over HTTPS in production
-      expires: 1.day.from_now # Set expiration if needed
+      expires: 1.day.from_now, # Set expiration if needed
       # same_site: :none
+      same_site: Rails.env.production? ? :none : :lax
     }
   end
 
